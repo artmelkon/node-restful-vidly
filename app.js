81d8be33ff0251bd94c738e44a2667f616e2a3a3
Arthur Melkonyan <arthur@artmelkon.me>
2020-03-25 19:37:10 -0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-const mogoose = require('mongoose');
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log('MongoDB Connected'))
-  .catch( err => console.loc(new Error('Error', err.message)))
+mongoose.connect('mongodb://localhost:27017/playground', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch( err => console.error('Error', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: 'Angular Course',
+    author: 'Arthur',
+    tags: ['angular', 'frontend' ],
+    isPublished: true
+  });
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
