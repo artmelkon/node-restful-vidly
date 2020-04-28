01bcca30e9b64baf5de7c1e3fc3fec7fce287986
Arthur Melkonyan <arthur@artmelkon.me>
2020-04-27 17:09:40 -0700

    update

--- controllers/movies.js
+++ controllers/movies.js
@@ -15,6 +15,14 @@ exports.postMovie = async (req, res) => {
   if(!genre) return res.status(400).send('Invalid genre');
 
   let movie = new Movie({
-    
-  })
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate
+  });
+  movie = await movie.save();
+  console.log(movie);
 }
