82332246569f4cd37855fa6f532456a17c8a93cb
Arthur <arthur@artmelkon.me>
2020-10-26 13:02:31 -0700

    update app.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,10 +8,10 @@ exports.postLogin = async (req, res) => {
   if(error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if(!user) return res.status(400).send('Invalid username or password');
+  if(!user) return res.status(400).send('Invalid email or password');
 
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if(!validPass) return res.status(400).send('Invalid username or password');
+  if(!validPass) return res.status(400).send('Invalid email or password');
 
   const token = user.generateAuthToken();
   res.send(token)
